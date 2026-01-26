bcc33e41e61c8b73457ac07f134694410742fa96
vikab <vikabashlykova2007@gmail.com>
2026-01-26 10:56:37 +0300

    связала с БД

---
+++ PR12/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR12
+{
+    internal class Core
+    {
+        public static PR14_CinemaEntities Context = new PR14_CinemaEntities();
+    }
+}
