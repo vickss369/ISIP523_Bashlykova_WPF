13660b924cb8142c1824fed20667c97d0f6c7f5d
vikab <vikabashlykova2007@gmail.com>
2026-01-26 12:08:16 +0300

    начало работы с фильмами

--- PR12/filmsPage.xaml.cs
+++ PR12/filmsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,9 @@ namespace PR12
         public filmsPage()
         {
             InitializeComponent();
+
+            AddFilms();
+            LoadFilms();
         }
 
         private void goToAkk_Click(object sender, RoutedEventArgs e)
@@ -30,9 +34,96 @@ namespace PR12
             NavigationService.Navigate(new accountPage());
         }
 
+        private void AddFilms()
+        {
+            if (!Core.Context.Films.Any())
+            {
+                var f1 = new Films
+                {
+                    FilmName = "Майор Гром: Игра",
+                    Description = "У бесстрашного защитника Петербурга майора Игоря Грома — новый враг, называющий себя Призраком. Он предлагает Грому сыграть в опасную игру, ставка в которой — жизни обычных людей.",
+                    FilmRating = 7.5,
+                    StartDate = new System.DateTime(),
+                    AgeRatingID = 4,
+                    ImagePath = "Images/grom.png"
+                };
+
+                var f2 = new Films
+                {
+                    FilmName = "Майор Гром: Игра",
+                    Description = "У бесстрашного защитника Петербурга майора Игоря Грома — новый враг, называющий себя Призраком. Он предлагает Грому сыграть в опасную игру, ставка в которой — жизни обычных людей.",
+                    FilmRating = 7.5,
+                    StartDate = new System.DateTime(),
+                    AgeRatingID = 4,
+                    ImagePath = "Images/grom.png"
+                };
+
+                var f3 = new Films
+                {
+                    FilmName = "Майор Гром: Игра",
+                    Description = "У бесстрашного защитника Петербурга майора Игоря Грома — новый враг, называющий себя Призраком. Он предлагает Грому сыграть в опасную игру, ставка в которой — жизни обычных людей.",
+                    FilmRating = 7.5,
+                    StartDate = new System.DateTime(),
+                    AgeRatingID = 4,
+                    ImagePath = "Images/grom.png"
+                };
+
+                var f4 = new Films
+                {
+                    FilmName = "Майор Гром: Игра",
+                    Description = "У бесстрашного защитника Петербурга майора Игоря Грома — новый враг, называющий себя Призраком. Он предлагает Грому сыграть в опасную игру, ставка в которой — жизни обычных людей.",
+                    FilmRating = 7.5,
+                    StartDate = new System.DateTime(),
+                    AgeRatingID = 4,
+                    ImagePath = "Images/grom.png"
+                };
+
+                var f5 = new Films
+                {
+                    FilmName = "Майор Гром: Игра",
+                    Description = "У бесстрашного защитника Петербурга майора Игоря Грома — новый враг, называющий себя Призраком. Он предлагает Грому сыграть в опасную игру, ставка в которой — жизни обычных людей.",
+                    FilmRating = 7.5,
+                    StartDate = new System.DateTime(),
+                    AgeRatingID = 4,
+                    ImagePath = "Images/grom.png"
+                };
+
+                var f6 = new Films
+                {
+                    FilmName = "Майор Гром: Игра",
+                    Description = "У бесстрашного защитника Петербурга майора Игоря Грома — новый враг, называющий себя Призраком. Он предлагает Грому сыграть в опасную игру, ставка в которой — жизни обычных людей.",
+                    FilmRating = 7.5,
+                    StartDate = new System.DateTime(),
+                    AgeRatingID = 4,
+                    ImagePath = "Images/grom.png"
+                };
+
+                Core.Context.Films.Add(f1);
+                Core.Context.Films.Add(f2);
+                Core.Context.Films.Add(f3);
+                Core.Context.Films.Add(f4);
+                Core.Context.Films.Add(f5);
+                Core.Context.Films.Add(f6);
+
+                Core.Context.SaveChanges();
+            }
+        }
+
+        private void LoadFilms()
+        {
+            var list = Core.Context.Films.ToList();
+            filmsList.ItemsSource = list;
+        }
+
+
         private void goToReg_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new registrationPage());
+        }
+
+        private void buyBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
