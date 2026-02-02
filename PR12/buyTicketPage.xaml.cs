8c298e77d5fb5f05457c5115eb3efe34ec364221
vikab <vikabashlykova2007@gmail.com>
2026-02-02 11:43:14 +0300

    оформление билета (пока без БД)

--- PR12/buyTicketPage.xaml.cs
+++ PR12/buyTicketPage.xaml.cs
@@ -20,9 +20,31 @@ namespace PR12
     /// </summary>
     public partial class buyTicketPage : Page
     {
+        private List<SessionPlace> selectedPlaces;
+        private Sessions currentSession;
+
         public buyTicketPage(List<SessionPlace> places, Sessions session)
         {
             InitializeComponent();
+
+            selectedPlaces = places;
+            currentSession = session;
+
+            LoadTicketInfo();
+        }
+
+        private void LoadTicketInfo()
+        {
+            FilmNameTB.Text = "Фильм: " + currentSession.Films.FilmName;
+            HallNumTB.Text = "Зал: " + currentSession.Halls.HallRating.Nomination;
+            SessionDateTB.Text = "Дата сеанса: " + currentSession.SessionDate.ToString("dd.MM.yyyy");
+
+            var placeNumbers = selectedPlaces.Select(p => p.Places.PlaceNumber).ToList();
+            SelectedPlaceTB.Text ="Места: " + string.Join(", ", placeNumbers);
+
+            double basePrice = currentSession.Halls.HallRating.PlacePrice;
+            double finalPrice = basePrice * 1.5 * selectedPlaces.Count;
+            PriceTB.Text = "Стоимость: " + finalPrice.ToString("0.00") + "₽";
         }
 
         private void goBack_Click(object sender, RoutedEventArgs e)
@@ -32,7 +54,17 @@ namespace PR12
 
         private void confirm_Click(object sender, RoutedEventArgs e)
         {
+            if (selectedPlaces.Count == 0)
+            {
+                MessageBox.Show("Выберите места!");
+                return;
+            }
+
+            MessageBox.Show("Билет успешно куплен!");
+            // тут сохранить в БД будет когда-то
+
             NavigationService.Navigate(new MainPage());
         }
     }
+
 }
